ca8c9c359235577a8294958f9f2ac44bb70b8288
oliver <oliver.wu@hinterlands.com.au>
2020-11-25 05:14:49 +0000

    update form with formix

--- src/pages/formWithCustomValidation.tsx
+++ src/pages/formWithCustomValidation.tsx
@@ -8,24 +8,10 @@ import {
 	initializeIcons
 } from '@fluentui/react';
 import * as yup from 'yup'
-import { getValidationErrors } from '../utils/help'
+import { useFormik } from 'formik';
 
 initializeIcons();
 
-const schema = yup.object().shape({
-	firstName: yup
-		.string()
-		.label("First Name")
-		.required(),
-	lastName: yup
-		.string()
-		.label("Last Name")
-		.required(),
-	gender: yup
-		.string()
-		.label("Gender")
-		.required(),	
-})
 
 const dropdownStyles: Partial<IDropdownStyles> = {
 	dropdown: { width: 300 },
@@ -37,129 +23,63 @@ const genderOptions: IDropdownOption[] = [
 	{ key: 'other', text: 'Other' },
 ];
 
-interface IValidationErrors {
-	firstName?: string,
-	lastName?: string,
-	gender?: string
-}
-
-const formValidation = async (userInput, schema) => {
-	let validationErrors: IValidationErrors = {}
-
-	try {
-		await schema.validate(userInput, {
-			abortEarly: false
-		});
-	} catch(e) {
-		validationErrors = getValidationErrors(e);
-	} finally {
-		return validationErrors
-	}
-}
-
 const Form: React.FunctionComponent = () => {
-	const [firstName, setFirstName] = React.useState('')
-	const [lastName, setLastName] = React.useState('')
-	const [gender, setGender] = React.useState('')
-	const [validationErrors, setValidationErrors] = React.useState<IValidationErrors>({})
-
-	
-
-	const onSubmit = async () => {
-		const userInput = {
-			firstName,
-			lastName,
-			gender
-		}
-
-		const validationErrors = await formValidation(userInput, schema)
-		setValidationErrors(validationErrors)
-		console.log(validationErrors)
-
-	};
-
-	const handleFirstNameChange = async (event, value: string) => {
-        setFirstName(value)
-	};
-
-	const handleFirstNameBlur = async () => {
-		const userInput = { firstName }
-		const validationErrors = await formValidation(userInput, schema)
-		const firstNameValidationErrors = validationErrors.firstName
-		setValidationErrors((validationErrors) => {
-			return {
-				...validationErrors,
-				firstName: firstNameValidationErrors
-			}
-		})
-	}
-	
-	const handleLastNameChange = (event, value: string) => {
-        setLastName(value)
-	};
-	
-	const handleLastNameBlur = async () => {
-		const userInput = { lastName }
-		const validationErrors = await formValidation(userInput, schema)
-		const lastNameValidationErrors = validationErrors.lastName
-		setValidationErrors((validationErrors) => {
-			return {
-				...validationErrors,
-				lastName: lastNameValidationErrors
-			}
-		})
-	}
-
-	const handleDropdownChange = (event, option) => {
-		console.log(option.key)
-		setGender(option.key)
-	}
-
-	const handleGenderBlur = async () => {
-		
-		const userInput = { gender }
-		const validationErrors = await formValidation(userInput, schema)
-		const genderValidationErrors = validationErrors.gender
-		setValidationErrors((validationErrors) => {
-			return {
-				...validationErrors,
-				gender: genderValidationErrors
-			}
-		})
-	}
+	const formik = useFormik({
+		initialValues: {
+			firstName: '',
+			lastName: '',
+			gender: ''
+		},
+		validationSchema: yup.object({
+			firstName: yup
+				.string()
+				.label("First Name")
+				.required(),
+			lastName: yup
+				.string()
+				.label("Last Name")
+				.required(),
+			gender: yup
+				.string()
+				.label("Gender")
+				.required(),	
+		}),
+		onSubmit: values => {
+		  	alert(JSON.stringify(values, null, 2));
+		},
+	});
 
 
 	return (
-		<form>
+		<form onSubmit={formik.handleSubmit}>
 			<TextField
 				label="First Name"
 				name="firstName"
-				value={firstName}
-				onBlur={handleFirstNameBlur}
-				onChange={handleFirstNameChange}
-				errorMessage={validationErrors.firstName}
+				onChange={formik.handleChange}
+				onBlur={formik.handleBlur}
+				value={formik.values.firstName}
+				errorMessage={formik.touched.firstName && formik.errors.firstName}
 			/>
 			<TextField
 				label="Last Name"
 				name="lastName"
-				value={lastName}
-				onBlur={handleLastNameBlur}
-				onChange={handleLastNameChange}
-				errorMessage={validationErrors.lastName}
+				onChange={formik.handleChange}
+				onBlur={formik.handleBlur}
+				value={formik.values.lastName}
+				errorMessage={formik.touched.lastName && formik.errors.lastName}
 			/>
 			<Dropdown
 				placeholder="Select an option"
 				label="Gender"
 				options={genderOptions}
 				styles={dropdownStyles}
-				// defaultSelectedKey ="other"
-				onChange={handleDropdownChange}
-				onBlur={handleGenderBlur}
-				errorMessage={validationErrors.gender}
+				errorMessage={formik.touched.gender && formik.errors.gender}
+				onBlur={() => formik.setFieldTouched('gender')}
+				onChange={(event, option) => formik.setFieldValue('gender', option.key)}
 			/>
 			<DefaultButton 
 				text="Standard" 
-				onClick={onSubmit}
+				type="submit"
 			/>
 
 		</form>
