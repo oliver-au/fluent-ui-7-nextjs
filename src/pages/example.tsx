95f8c0f9264d810f1e952d3fbfe63eabb1e9c427
oliver <oliver.wu@hinterlands.com.au>
2020-11-25 09:48:08 +0000

    add font apply to example

--- src/pages/example.tsx
+++ src/pages/example.tsx
@@ -1,13 +1,28 @@
 import React from 'react'
 import MyStyledComponent from '../components/MyStyledComponent'
+import { mergeStyleSets } from '@uifabric/merge-styles';
+
+export interface IComponentClassNames {
+	root: string;
+}
+
+const getClassNames = (): IComponentClassNames => {
+	
+	return mergeStyleSets({
+		root: {
+			fontFamily: 'Proxima Nova'
+		},
+	});
+};
 
 const Example = () => {
+	const { root } = getClassNames();
 
 	return (
-		<>
+		<div className={root}>
 			<h1>My Customizing Component Example</h1>
 			<MyStyledComponent/>
-		</>
+		</div>
 	)
 }
 
--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -1,6 +1,7 @@
 import porximaNovaRegularWoff2 from '../assets/fonts/proxima-nova-regular.woff2'
+import { IFontFace } from '@uifabric/merge-styles'
 
-export const porximaNovaRegular = {
+export const porximaNovaRegular: IFontFace = {
 	fontFamily: `"Proxima Nova"`,
 	src: `url(${porximaNovaRegularWoff2}) format('woff2')`,
 	fontWeight: '400',
