77855bdd3d5445d461af28acb86a16dd37f9198c
oliver <oliver.wu@hinterlands.com.au>
2020-11-30 02:11:50 +0000

    alter a child area by interacting with the parent

--- src/components/MyStyledComponent.tsx
+++ src/components/MyStyledComponent.tsx
@@ -12,52 +12,97 @@ export interface IComponentClassNames {
 	buttonIcon: string;
 	parent: string;
 	child2: string;
+	parent3: string;
+	child3: string;
+	child4: string;
 }
 
+const classNames = {
+	parent3: 'My-paren-3',
+	child4: 'My-Child-4'
+};
+
 const getClassNames = (): IComponentClassNames => {
-	const mdMediaQuery = `@media(max-width: ${variable.md}px)`
 
 	return mergeStyleSets({
-		root: {
+		root: [
+			{
 			background: 'red',
-		},
+			}
+		],
 
-		button: {
-			backgroundColor: 'green',
-			// Basic pseudo-selectors
-			':hover': {
-				background: 'blue'
-			},
-			// Media and feature queries
-			mdMediaQuery: {
-				background: 'green',
-			},
-		},
+		button: [
+			{
+				backgroundColor: 'green',
+				// Basic pseudo-selectors
+				':hover': {
+					background: 'blue'
+				},
+				// Media and feature queries
+				[`@media(max-width: ${variable.md}px)`]: {
+					background: 'green',
+				},
+			}
+		],
 
-		buttonIcon: {
-			margin: 10,
-		},
+		buttonIcon: [
+			{
+				margin: 10,
+			}
+		],
 
 		// Parent/child selectors
-		parent: {
-			backgroundColor: theme.palette.themePrimary,
-			// May use for numerous li in ul
-			'& .child1': {
-				background: theme.palette.themeSecondary,
-				display: 'inline-block',
-				fontFamily: 'Proxima Nova'
+		parent: [
+			{
+				backgroundColor: theme.palette.themePrimary,
+				// May use for numerous li in ul
+				'& .child1': {
+					background: theme.palette.themeSecondary,
+					display: 'inline-block',
+					fontFamily: 'Proxima Nova'
+				}
 			}
-		},
-		child2: {
-			backgroundColor: theme.palette.themeTertiary,
-			display: 'inline-block'
-		}
+		],
+		child2: [
+			{
+				backgroundColor: theme.palette.themeTertiary,
+				display: 'inline-block'
+			}
+		],
+
+		parent3: [
+			classNames.parent3,
+			{
+				'& > li': {
+					backgroundColor: 'green'
+				}
+			}
+		],
+
+		child3: [
+			{
+				backgroundColor: 'blue',
+				':hover': {
+					background: 'white'
+				},
+			}
+		],
+		
+		child4: [
+			classNames.child4,
+			{
+			  [`.${classNames.parent3}:hover &`]: {
+				background: 'green',
+			  },
+			},
+		],
+
 	});
 };
 
 
 const MyStyledComponent = () => {
-	let { root, button, buttonIcon, parent, child2 } = getClassNames();
+	let { root, button, buttonIcon, parent, child2, parent3, child3, child4 } = getClassNames();
 
 	return (
 		<div className={root}>
@@ -71,6 +116,16 @@ const MyStyledComponent = () => {
 				<div className={child2}>
 					Child2
 				</div>
+			</div>
+			<div className={parent3}>
+				<div className={child3}>
+					Child3
+				</div>
+				<div className={child4}>
+					Child4
+				</div>
+				<li>li1</li>
+				<li>li2</li>
 			</div>
 		</div>
 	);
